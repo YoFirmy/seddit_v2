7ba3b4701fd300eb848bfa3f257ac21e5281c742
YoFirmy <mattfirmston@gmail.com>
2021-12-20 16:28:27 +0000

    fully styled nav bar for desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import '../styles/Header.css';
 
+import dropDownImg from '../images/drop-down.svg'
+import headerLogo from '../images/headerLogo.svg'
+
 export default function Header() {
   return (
     <header>
-      <div className="logo">seddit</div>
-      <input type="text" placeholder='Search Seddit' />
-      <div className="right-side-buttons">
+      <div id="logo">
+        <img id="header-logo" src={headerLogo} alt="Seddit Logo" />
+      </div>
+      <input id="search-bar" type="text" placeholder='Search Seddit' />
+      <div id="right-side-buttons">
         <button className='btn btn-primary'>Log in</button>
         <button className='btn btn-secondary'>Sign up</button>
-        <button>drop-down</button>
+        <button id="btn-drop-down"><img id="drop-down-image" src={dropDownImg} alt="drop down menu" /></button>
       </div>
     </header>
   )
