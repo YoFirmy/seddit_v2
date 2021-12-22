eae55e69374610f1ea90f972792ef27327bf5e24
YoFirmy <mattfirmston@gmail.com>
2021-12-22 09:45:25 +0000

    changed css to use classes rather than id

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,15 +6,15 @@ import headerLogo from '../../images/headerLogo.svg'
 export default function Header() {
   return (
     <header>
-      <div id="logo">
-        <img id="header-logo" src={headerLogo} alt="Seddit Logo" />
+      <div className="logo">
+        <img className="header-logo" src={headerLogo} alt="Seddit Logo" />
       </div>
-      <input id="search-bar" type="text" placeholder='Search Seddit' />
-      <div id="right-side-buttons">
+      <input className="search-bar" type="text" placeholder='Search Seddit' />
+      <div className="right-side-buttons">
         <button className='btn btn-primary'>Log in</button>
         <button className='btn btn-secondary'>Sign up</button>
-        <button id="btn-drop-down">
-          <img id="drop-down-image" src={dropDownImg} alt="drop down menu" />
+        <button className="btn-drop-down">
+          <img className="drop-down-image" src={dropDownImg} alt="drop down menu" />
         </button>
       </div>
     </header>
